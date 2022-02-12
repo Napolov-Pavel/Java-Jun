33ea11ad663019ad2eaa328bc3bb642c94c23157
Napolov Pavel <Napolov.pavel@yandex.ru>
2022-02-12 18:41:10 +0300

    add Filter test

---
+++ src/test/java/com/gridnine/testing/filters/FilterTest.java
@@ -0,0 +1,28 @@
+package com.gridnine.testing.filters;
+
+import com.gridnine.testing.test.Flight;
+import com.gridnine.testing.test.Segment;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FilterTest {
+    private Filter sut;
+
+    @Test
+    void shouldProperlyDepartureBeforeCurrentTime() {
+        Segment segment = new Segment(LocalDateTime.now().minusDays(1), LocalDateTime.now());
+        List<Segment> listTest = new ArrayList<>();
+        listTest.add(segment);
+        Flight flight = new Flight(listTest);
+        sut = new DepartureBeforeCurrentTime();
+
+        boolean result = sut.check(flight);
+
+        assertTrue(result);
+    }
+}
